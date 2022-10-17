ba11d1212e4ee89d1ad3b461065cb19f9cdcfe05
dilan <dilan@Dilan>
2022-10-16 19:29:56 -0500

    v1

---
+++ src/logica/Calculador.java
@@ -0,0 +1,21 @@
+
+package logica;
+
+import gui.Calculadora;
+
+/**
+ *
+ * @author dilan
+ */
+public class Calculador {
+
+ 
+    public static void main(String[] args) {
+       
+            Calculadora calu = new Calculadora();
+            calu.setVisible(true);
+            calu.setLocationRelativeTo(null);
+        
+    }
+    
+}
